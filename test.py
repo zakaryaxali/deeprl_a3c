af7075227891e4a2ef51bc9b2bc2bce3bdf7336e
Quentin Spalla <quentin.spalla@gmail.com>
2018-02-04 18:12:33 +0100

    Forgot this

--- test.py
+++ test.py
@@ -5,46 +5,28 @@
 Test File
 """
 
-from layer import ConvLayer, FCLayer, FlattenLayer, ReLULayer, SoftmaxLayer
-from player import ActorA3C
 import constants 
+from async import SharedWeights
+import multiprocessing as mp
+from player import create_player_atari
 
-player_thread = ActorA3C(game_name=constants.ROM, 
-                         rand_seed=constants.INITIAL_SEED, 
-                         gamma=constants.GAMMA)
+all_players = []
+train_threads = []
 
-lay_conv1 = ConvLayer(input_channel=constants.SKIPED_FRAMES, 
-                      output_channel=constants.CONV1_FILTERS, 
-                      kernel_size=constants.CONV1_SIZE, 
-                      stride=constants.CONV1_STRIDE)
-
-lay_conv2 = ConvLayer(input_channel=constants.CONV1_FILTERS, 
-                      output_channel=constants.CONV2_FILTERS, 
-                      kernel_size=constants.CONV2_SIZE, 
-                      stride=constants.CONV2_STRIDE)
-
-lay_fc3 = FCLayer(constants.FC_LSTM_UNITS, constants.FC_LSTM_OUTPUTS)
-
-lay_fc4 = FCLayer(constants.FC_PI_UNITS, 
-                  len(player_thread.game_state.real_actions))
-
-lay_fc5 = FCLayer(constants.FC_V_UNITS, constants.FC_V_OUTPUTS)
-
-player_thread.local_network.add_layer(lay_conv1, constants.CONV1_POS)
-player_thread.local_network.add_layer(ReLULayer(), constants.RELU1_POS)
-
-player_thread.local_network.add_layer(lay_conv2, constants.CONV2_POS)
-player_thread.local_network.add_layer(ReLULayer(), constants.RELU2_POS)
-
-player_thread.local_network.add_layer(FlattenLayer(), constants.FLATTEN_POS)
-
-player_thread.local_network.add_layer(lay_fc3, constants.FC_LSTM_POS)
-player_thread.local_network.add_layer(ReLULayer(), constants.RELU3_POS)
-
-player_thread.local_network.add_layer(lay_fc4, constants.FC_PI_POS)
-player_thread.local_network.add_layer(SoftmaxLayer(), constants.SM_POS)
-
-player_thread.local_network.add_layer(lay_fc5, constants.FC_V_POS)
+all_players.append(create_player_atari(0))
+vect_weights_bias= all_players[0].local_network.get_all_weights_bias()
+sw = SharedWeights(constants.LEARNING_RATE, vect_weights_bias)
 
 
-player_thread.process(constants.T_MAX, constants.NBR_STEPS)
+for idx_thread in range(constants.PARALLEL_THREADS):
+    if idx_thread != 0:
+        all_players[idx_thread] = create_player_atari(idx_thread, False)
+    
+    train_threads[idx_thread] = mp.Process(target=all_players[idx_thread].process 
+                                    ,args=(constants.T_Max, constants.NBR_STEPS, sw))
+    
+for p in train_threads:
+    p.start()
+        
+for p in train_threads:
+    p.join()
