ce8123750080301dfa84c918415013362265afae
Zakarya Ali <zakarya.ali@gmail.com>
2018-02-05 21:02:16 +0100

    remove test_weights.py

--- test_weights.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-#Load pickle file
-input_file = 'a3c_weights.pkl' #sys.argv[1]
-pkl_file = open(input_file, 'rb')
-pkl = pickle.load(pkl_file)
-weights = pkl["weights"]
-pkl_file.close()
